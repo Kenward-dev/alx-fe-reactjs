f566759540fff6654240f08fb07aba17d6ffe50f
Kenward Terhemba <codewithkenward@gmail.com>
2025-09-05 13:57:09 +0100

    add inline styles to components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer style={{ textAlign: "center", marginTop: "20px" }}>
+        <p style={{ color: "pink" }}>© 2023 City Lovers</p>
+        </footer>
+
+    )
+}
+
+export default Footer;
